2ef005e3e95007b1a9a3112c4051eb29008b5a2f
Stepan Kolodkin <zorokon@yandex.ru>
2023-02-26 16:09:27 +0300

    change App

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export const App = () => {
+    const [todo, setTodo] = useState("");
+
+    return (
+        <div className="App">
+            <h1>React Todo App</h1>
+
+            <div className="input-wrapper">
+                <input
+                    type="text"
+                    name="todo"
+                    placeholder="Create a new todo"
+                    onChange={(event) => {
+                        setTodo(event.target.value);
+                    }}
+                />
+                <button className="add-button">Add</button>
+            </div>
+        </div>
+    );
+};
