0cb2ef20000939b0badb806cab1ab260f15c3854
Stepan Kolodkin <zorokon@yandex.ru>
2023-02-26 21:09:12 +0300

    finish

---
+++ to-do/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import "./App.css";
+
+
+export const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+
+    const addTodo = () => {
+        if (todo !== "") {
+            setTodos([...todos, todo]);
+            setTodo("");
+        }
+    };
+
+    const deleteTodo = (text) => {
+        const newTodos = todos.filter((todo) => {
+            return todo !== text;
+        });
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="App">
+            <h1>React Todo App</h1>
+
+            <div className="input-wrapper">
+                <input
+                    type="text"
+                    name="todo"
+                    value={todo}
+                    placeholder="Create a new task"
+                    onChange={(e) => {
+                        setTodo(e.target.value);
+                    }}
+                />
+                <button className="add-button" onClick={addTodo}>
+                    Add
+                </button>
+            </div>
+
+            {todos?.length > 0 ? (
+                <ul className="todo-list">
+                    {todos.map((todo, index) => (
+                        <div className="todo">
+                            <li key={index}> {todo} </li>
+
+                            <button
+                                className="delete-button"
+                                onClick={() => {deleteTodo(todo);}}
+                            >
+                                Delete
+                            </button>
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <div className="empty">
+                    <p>No task found</p>
+                </div>
+            )}
+        </div>
+    );
+};
