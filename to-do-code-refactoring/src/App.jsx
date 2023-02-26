1e65fff47c320ea02d66ecd1a75e26efbaeacf26
Stepan Kolodkin <zorokon@yandex.ru>
2023-02-26 21:23:33 +0300

    to-do-refactoring-code

---
+++ to-do-code-refactoring/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useState } from "react";
+import { TodoInput } from "./TodoInput";
+import { TodoList } from "./TodoList";
+import "./App.css";
+
+
+export const App = () => {
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+
+    const addTodo = () => {
+        if (todo !== "") {
+            setTodos([...todos, todo]);
+            setTodo("");
+        }
+    };
+
+    const deleteTodo = (text) => {
+        const newTodos = todos.filter((todo) => {
+            return todo !== text;
+        });
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="App">
+            <h1>React Todo App</h1>
+            <TodoInput todo={todo} setTodo={setTodo} addTodo={addTodo} />
+            <TodoList list={todos} remove={deleteTodo} />
+        </div>
+    );
+};
