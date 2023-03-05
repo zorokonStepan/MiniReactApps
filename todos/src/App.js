e47ce790d02e1ca40d608c072770afee4b865fca
Stepan Kolodkin <zorokon@yandex.ru>
2023-03-05 09:16:31 +0300

    change App.js

--- todos/src/App.js
+++ todos/src/App.js
@@ -3,12 +3,12 @@ import { Component } from 'react';
 const date1 = new Date(2023, 3, 4, 20, 15);
 const date2 = new Date(2023, 3, 4, 22, 22);
 
-const initialDate = [  // ...................................................1
+const initialDate = [
   {
     title: 'Задача №1',
     desc: 'Срочно',
     image: '',
-    done: true,  //   .......................................................2
+    done: true,
     createAt: date1.toLocaleString(),
     key: date1.getTime()
   },
@@ -22,26 +22,26 @@ const initialDate = [  // ...................................................1
   },
 ];
 
-export default class App extends Component {  // ............................3
-  constructor(props) {  // ..................................................4
-    super(props);  //  ......................................................5
-    this.data = initialDate;  // ............................................6
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.data = initialDate;
   }
 
-  render() {  // ............................................................7
-    return (  // ............................................................8
-        <div>  {/*..........................................................10 */}
-          <nav className="navbar is-light">  {/* ...........................11 */}
-            <div className="navbar-brand">   {/* ...........................12 */}
-              <span className="navbar-item is-upercase">  {/* ..............13 */}
+  render() {
+    return (
+        <div>
+          <nav className="navbar is-light">
+            <div className="navbar-brand">
+              <span className="navbar-item is-upercase">
                 Todos
               </span>
             </div>
           </nav>
-          <main className="context px-6 mt-6">  {/* ...........................14 */}
+          <main className="context px-6 mt-6">
             <h1>Todos</h1>
           </main>
         </div>
-    );  // .....................................................................9
+    );
   }
 }
